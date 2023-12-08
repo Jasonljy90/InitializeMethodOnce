84645538386ac2f0189f6b81ef53eb68346e603e
Jasonljy90 <79128355+Jasonljy90@users.noreply.github.com>
2023-12-08 14:35:14 +0800

    update message

--- src/main/java/com/jason/Main.java
+++ src/main/java/com/jason/Main.java
@@ -19,32 +19,32 @@ public class Main {
 //
 //        SynchronizedInitializer synchronizedInitializer = new SynchronizedInitializer();
 //
-//        System.out.println("Before: " + synchronizedInitializer.callCount);
+//        System.out.println("Before Initialize Count: " + synchronizedInitializer.callCount);
 //        synchronizedInitializer.initialize();
-//        System.out.println("After First: " + synchronizedInitializer.callCount);
+//        System.out.println("After First Initialize Count: " + synchronizedInitializer.callCount);
 //        synchronizedInitializer.initialize();
-//        System.out.println("After Second: " + synchronizedInitializer.callCount);
+//        System.out.println("After Second Initialize Count: " + synchronizedInitializer.callCount);
 //        synchronizedInitializer.initialize();
-//        System.out.println("After Third: " + synchronizedInitializer.callCount);
+//        System.out.println("After Third Initialize Count: " + synchronizedInitializer.callCount);
 //    }
 
 //    public static void main(String[] args) {
 //
-//        System.out.println("First: " + StaticInitializer.CALL_COUNT);
-//        System.out.println("Second: " + StaticInitializer.CALL_COUNT);
+//        System.out.println("First Initialize Count: " + StaticInitializer.CALL_COUNT);
+//        System.out.println("Second Initialize Count: " + StaticInitializer.CALL_COUNT);
 //    }
 
         public static void main(String[] args) {
 
         SynchronizedInitializer synchronizedInitializer = new SynchronizedInitializer();
 
-        System.out.println("Before: " + synchronizedInitializer.callCount);
+        System.out.println("Before Initialize Count: " + synchronizedInitializer.callCount);
         synchronizedInitializer.initialize();
-        System.out.println("After First: " + synchronizedInitializer.callCount);
+        System.out.println("After First Initialize Count: " + synchronizedInitializer.callCount);
         synchronizedInitializer.initialize();
-        System.out.println("After Second: " + synchronizedInitializer.callCount);
+        System.out.println("After Second Initialize Count: " + synchronizedInitializer.callCount);
         synchronizedInitializer.initialize();
-        System.out.println("After Third: " + synchronizedInitializer.callCount);
+        System.out.println("After Third Initialize Count: " + synchronizedInitializer.callCount);
     }
 
 }
